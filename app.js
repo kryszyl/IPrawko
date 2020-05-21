924e9d8ff83b43b99d3724c2dfc655e889af04fa
Dawid Kryszyłowicz <dawid@MacBook-Pro-Dawid.local>
2020-05-21 20:23:45 +0200

    refactored setStickyNavbar

--- app.js
+++ app.js
@@ -1,49 +1,46 @@
 const navSlide = () => {
-    const burger = document.querySelector('.nav__burger');
-    const nav = document.querySelector('.nav__list');
-    const navItems = document.querySelectorAll('.nav__item');
-    const burgerLines = document.querySelectorAll('.nav__burger div');
+  const burger = document.querySelector('.nav__burger');
+  const nav = document.querySelector('.nav__list');
+  const navItems = document.querySelectorAll('.nav__item');
+  const burgerLines = document.querySelectorAll('.nav__burger div');
 
+  burger.addEventListener('click', () => {
+    //toggle mobile nav
+    nav.classList.toggle('nav__list--active');
 
-    burger.addEventListener('click', () => {
-        //toggle mobile nav
-        nav.classList.toggle('nav__list--active');
-        
-        //links animation
-        navItems.forEach((item, index) => {
-            if (item.style.animation) {
-                item.style.animation = ``;
-            }else {
-                item.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-
-        });
-        // burger animation
-         
-        burgerLines.forEach((line, index) => {
-            console.log(line.classList);
-         
-            line.classList.toggle(`burger__line${index+1}--toggle`);
-        })
+    //links animation
+    navItems.forEach((item, index) => {
+      if (item.style.animation) {
+        item.style.animation = ``;
+      } else {
+        item.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
     });
+    // burger animation
 
+    burgerLines.forEach((line, index) => {
+      console.log(line.classList);
 
-}
+      line.classList.toggle(`burger__line${index + 1}--toggle`);
+    });
+  });
+};
 
 const setStickyNavbar = () => {
-    window.addEventListener('scroll', ()=>{
-        if(window.scrollY > 0){
-            document.querySelector('nav').classList.toggle('sticky', window.scrollY > 0);
-            document.querySelector('.banner').style.paddingTop = '20vh';
-        } else if (window.scrollY <= 0){
-            document.querySelector('nav').classList.remove('sticky');
-            document.querySelector('.banner').style.paddingTop = '0vh';
-        }
-    })
-}
-
-
+  const nav = document.querySelector('nav');
+  const banner = document.querySelector('.banner');
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      nav.classList.toggle('sticky', window.scrollY > 0);
+      banner.style.paddingTop = '20vh';
+    } else if (window.scrollY <= 0) {
+      nav.classList.remove('sticky');
+      banner.style.paddingTop = '0vh';
+    }
+  });
+};
 
 navSlide();
 setStickyNavbar();
-
