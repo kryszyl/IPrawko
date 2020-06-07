32ca21d0c51bf1008767f0bb15891d25e2fdd151
Dawid Kryszyłowicz <dawid@MacBook-Pro-Dawid.local>
2020-06-08 00:14:16 +0200

    added gallery carousel behaviour

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 const cards = document.querySelectorAll('.about__card');
 const aboutHeader = document.querySelector('.about__header');
 const pricesHeader = document.querySelector('.prices__header');
-const pricesContainer = document.querySelector('.prices__container')
+const pricesContainer = document.querySelector('.prices__container');
+const gallerySlide = document.querySelector('.gallery__slide');
+const galleryImgs = document.querySelectorAll('.gallery__photo');
+const btnPrev = document.querySelector('#btnPrev');
+const btnNext = document.querySelector('#btnNext');
 
 const isInViewport = (element) => {
   const rect = element.getBoundingClientRect();
   const html = document.documentElement;
 
-  return rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight && html.clientHeight) && 
-        rect.right <= (window.innerWidth && html.clientWidth);
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight && html.clientHeight) &&
+    rect.right <= (window.innerWidth && html.clientWidth)
+  );
 };
 
 const animateCardsInView = () => {
-  window.addEventListener('scroll', () => {        
-        cards.forEach(c => {
-          if (isInViewport(c)) {
-            console.log('in view')
-            c.style.opacity = '1'
-          }
-          else {
-            c.style.opacity = '0'
-            console.log('not in view')
-          }
-        });
-      });
+  window.addEventListener('scroll', () => {
+    cards.forEach((c) => {
+      if (isInViewport(c)) {
+        console.log('in view');
+        c.style.opacity = '1';
+      } else {
+        c.style.opacity = '0';
+        console.log('not in view');
+      }
+    });
+  });
 };
 
 const animatePricesInView = () => {
@@ -36,7 +41,7 @@ const animatePricesInView = () => {
       pricesContainer.style.transform = 'translateX(-100vw)';
     }
   });
-}
+};
 
 const navSlide = () => {
   const burger = document.querySelector('.nav__burger');
@@ -50,7 +55,6 @@ const navSlide = () => {
 
     //links animation
 
-
     // burger animation
 
     burgerLines.forEach((line, index) => {
@@ -59,25 +63,23 @@ const navSlide = () => {
       line.classList.toggle(`burger__line${index + 1}--toggle`);
     });
 
-      navItems.forEach((item, index) => {
+    navItems.forEach((item, index) => {
+      if (item.style.animation) {
+        item.style.animation = ``;
+      } else {
+        item.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
 
-          
-        if (item.style.animation) {
-          item.style.animation = ``
-        } else {
-          item.style.animation = `navLinkFade 0.5s ease forwards ${
-            index / 7 + 0.3
-          }s`;
-        };
-
-        item.addEventListener('click', () => {
-          nav.classList.remove('nav__list--active')
-          navItems.forEach(item => item.style.animation = ``);
-          burgerLines.forEach((line, index) => {
+      item.addEventListener('click', () => {
+        nav.classList.remove('nav__list--active');
+        navItems.forEach((item) => (item.style.animation = ``));
+        burgerLines.forEach((line, index) => {
           line.classList.remove(`burger__line${index + 1}--toggle`);
-          });
+        });
+      });
     });
-  });
   });
 };
 
@@ -95,19 +97,43 @@ const setStickyNavbar = () => {
   });
 };
 
-// const slideCarousel = () => {
-//   document.querySelector('.gallery button').addEventListener('click', () => {
-//     if () {
-      
-//     } else {
-      
-//     }
-//   });
-// };
+const slideCarousel = () => {
+  let counter = 1;
+  const size = galleryImgs[0].clientWidth;
+  console.log(size);
+  gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  btnNext.addEventListener('click', () => {
+    if(counter >= galleryImgs.length - 1) return;
+    gallerySlide.style.transition = 'transform 0.4s ease-in-out';
+    counter++;
+    console.log(counter);
+    gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  });
 
+  btnPrev.addEventListener('click', () => {
+    if (counter <= 0) return;
+    gallerySlide.style.transition = 'transform 0.4s ease-in-out';
+    counter--;
+    console.log(counter);
+    gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+  });
 
+  gallerySlide.addEventListener('transitionend', () => {
+    if (galleryImgs[counter].id === 'last-clone') {
+      gallerySlide.style.transition = 'none';
+      counter = galleryImgs.length - 2;
+      gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    else if (galleryImgs[counter].id === 'first-clone') {
+      gallerySlide.style.transition = 'none';
+      counter = galleryImgs.length - counter;
+      gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+  });
+};
 
 animateCardsInView();
 animatePricesInView();
 navSlide();
 setStickyNavbar();
+slideCarousel();
